03b1fb069a829d7b8af9739a435f383a52e469ac
Oliver Blaser <58989552+oblaser@users.noreply.github.com>
2024-12-19 02:38:58 +0100

    initial commit

---
+++ src/middleware/sha1.cpp
@@ -0,0 +1,13 @@
+/*
+author          Oliver Blaser
+date            19.12.2024
+copyright       GPL-3.0 - Copyright (c) 2024 Oliver Blaser
+*/
+
+#include <cstddef>
+#include <cstdint>
+
+#include "sha1.h"
+
+
+namespace {}
---
+++ src/middleware/sha1.h
@@ -0,0 +1,20 @@
+/*
+author          Oliver Blaser
+date            19.12.2024
+copyright       GPL-3.0 - Copyright (c) 2024 Oliver Blaser
+*/
+
+/*
+    This SHA1 implementation is based on https://github.com/clibs/sha1 (100% Public Domain code).
+    See also https://github.com/vog/sha1, a header only C++ implementation of clibs/sha1.
+*/
+
+#ifndef IG_MIDDLEWARE_SHA1_H
+#define IG_MIDDLEWARE_SHA1_H
+
+#include <cstddef>
+#include <cstdint>
+
+
+
+#endif // IG_MIDDLEWARE_SHA1_H
